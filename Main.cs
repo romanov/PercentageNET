25b4a01446f91ad807a2e2e288afc7b0a037bd84
Yury Romanov <romanov@outlook.com>
2022-10-24 16:25:38 +0300

    Extensions

--- Main.cs
+++ Main.cs
@@ -3,8 +3,11 @@ using System.Diagnostics.Contracts;
 
 namespace PercentageNET;
 
+
+
 public static class PercentageHelper
 {
+
     public enum AddMode
     {
         Simple,
@@ -42,10 +45,25 @@ public static class PercentageHelper
             // upcharging the input amount by percentage
             // for example: you want to add "real" 20% to 1000 = 1250
             // 1250 - 20% = 1000
-            decimal upchargeAmount  = number / (1 - percentageAmount / 100);
+            decimal upchargeAmount = number / (1 - percentageAmount / 100);
             return upchargeAmount;
         }
 
     }
+
+    public static decimal Compare(decimal compareTo, decimal secondNumber)
+    {
+        if (compareTo == 0 || secondNumber == 0)
+            throw new ArgumentNullException();
+
+        decimal amount = ((compareTo - secondNumber) / Math.Abs(compareTo)) * 100;
+        return amount;
+    }
+
+    // Extension methods
+
+    public static decimal CountPercentage(this decimal inputNumber, decimal percentageAmount) => Count(inputNumber, percentageAmount);
+    public static decimal SubstractPercentage(this decimal inputNumber, decimal percentageAmount) => Substract(inputNumber, percentageAmount);
+    public static decimal AddPercentage(this decimal inputNumber, decimal percentageAmount, AddMode mode = AddMode.Simple) => Add(inputNumber, percentageAmount, mode);
 
 }
